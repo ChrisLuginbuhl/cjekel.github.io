a0bc8edf6ff97437035a13ae476bc04fd2448eef
Chris L <chris.luginbuhl@gmail.com>
2019-04-01 19:20:48 -0400

    Add command line parameters

--- facenet_bust.py
+++ facenet_bust.py
@@ -10,7 +10,7 @@
 # Clone or upload facenet_bust.py
 # Put images in subfolder /early_renaissance
 # Mkdir early_renaissance/outputs
-# Python facenet_bust.py
+# Python facenet_bust.py -i inPath -o outPath
 
 
 #   import facenet libraires
@@ -24,10 +24,10 @@ import tensorflow as tf
 import os
 import align.detect_face
 from PIL import Image
-
-#  import other libraries
 import cv2
-#import matplotlib.pyplot as plt
+import argparse
+import tqdm
+
 
 #   setup facenet parameters
 gpu_memory_fraction = 1.0
@@ -35,26 +35,37 @@ minsize = 50 # minimum size of face
 threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
 factor = 0.709 # scale factor
 
-wExpansion = 1.25   # fraction of face bounding box width to add to width. int or float is fine
-hExpansionUp = 0    # fraction of face boudning box height to add to top
-hExpansionDown = 0.9
+wExpansion = 1.35   # fraction of face bounding box width to add to width. int or float is fine
+hExpansionUp = 0.35    # fraction of face boudning box height to add to top
+hExpansionDown = 0.85
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--inpath", help="Path to json file from nounproject if not using API", default = None)
+parser.add_argument("-o", "--outpath", help="Path to folder of downloaded images (will be created if it doesn't exist), default = args.category", default = None)
+args = parser.parse_args()
+
+print()
+print( "Input Path {}, Output Path {}".format(
+        args.inpath,
+        args.outpath
+        ))
+print()
+
+inPath = os.path.normpath(args.inpath)
+outPath = os.path.normpath(args.outpath)
+os.makedirs(outPath, exist_ok=True)
 
 #   fetch images
-#image_dir = 'presidents/'
-image_dir = 'early_renaissance/'
-
 #   create a list of your images
 filenames = []
-images = os.listdir(image_dir)
+images = os.listdir(inPath)
 print("Num images: ", len(images))
 for filename in images:
     if filename.endswith( ('.jpeg', '.jpg', '.png', '.gif') ): # whatever file types you're using...
         filenames.append(filename)
 filenames.sort()
-
-print(filenames)
 print("Num filenames: ", len(filenames))
+print("First 5 filenames: " + str(filenames[:4]))
 
 #   Start code from facenet/src/compare.py
 print('Creating networks and loading parameters')
@@ -69,8 +80,8 @@ with tf.Graph().as_default():
 #   end code from facenet/src/compare.py
 
     for i in filenames:
-        input_path = image_dir + i
-        img = misc.imread(os.path.expanduser(input_path))
+        pathToFile = os.path.join(inPath, i)
+        img = misc.imread(os.path.expanduser(pathToFile))
         f, e = os.path.splitext(i)
         #   run detect_face from the facenet library
         bounding_boxes, _ = align.detect_face.detect_face(
@@ -79,7 +90,6 @@ with tf.Graph().as_default():
         # make a separate file for each detected face
         k = 0
         for (x1, y1, x2, y2, acc) in bounding_boxes:
-            print(i, int(x1), int(x2), int(y1), int(y2))
             bustX1 = int(x1 - int((x2-x1) * wExpansion / 2))  # calculate larger bounding boxes for bust
             bustX2 = int(x2 + int((x2 - x1) * wExpansion / 2))
             bustY1 = int(y1 - (y2 - y1) * hExpansionUp)
@@ -87,9 +97,9 @@ with tf.Graph().as_default():
             print(i, bustX1, bustX2, bustY1, bustY2)
             w = bustX2 - bustX1
             h = bustY2 - bustY1
-            im = Image.open(os.path.expanduser(image_dir + i))
+            im = Image.open(os.path.expanduser(pathToFile))
             new_im = Image.new("RGB", (h, h))  # make a square image, assuming height is always the largest dimension
             box = (bustX1, bustY1, bustX2, bustY2)
             new_im.paste(im.crop(box), (int((h-w)/2), 0))  # 2nd parameter is where to paste in x,y - centre the image l-R
-            new_im.save('early_renaissance/outputs/' + f + '_' + str(k) + '.jpg', 'JPEG', quality=70)
+            new_im.save(os.path.join(outPath, f) + '_' + str(k) + '.jpg', 'JPEG', quality=70)
             k += 1
